ee64fcb70a7dc0fcdb4d56ac91bfe7bd7404f0b4
Juan Pablo Restrepo <juanresco@unisabana.edu.co>
2024-04-11 10:42:24 -0500

    readme v 0.1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
